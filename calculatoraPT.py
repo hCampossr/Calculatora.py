d32044152ac3c9125ec6a98c4f49277b391c3cbb
Henrique Campos <henriquecamposc2006@gmail.com>
2026-05-28 14:46:26 -0300

    refactor: replaces ifs with elifs

--- calculatoraPT.py
+++ calculatoraPT.py
@@ -21,8 +21,7 @@ print("\n\n Selecione para a operação:\n",
     "1. Somar\n"
     "2. Subtrair\n"
     "3. Multiplicar\n"
-    "4. Dividir\n"
-    "5. Raiz quadrada\n\n"
+    "4. Dividir\n\n"
     
     "0. Sair\n")
 
@@ -35,19 +34,19 @@ if operacao_menu == 1:
     result = somar(num1, num2)
     print(num1, "+", num2, "=", result)
 
-if operacao_menu == 2:
+elif operacao_menu == 2:
     num1 = float(input("Digite os numeros da subtração: "))
     num2 = float(input())
     result = subtrair(num1, num2)
     print(num1, "-", num2, "=", result)
 
-if operacao_menu == 3:
+elif operacao_menu == 3:
     num1 = float(input("Digite os numeros da multiplicar: "))
     num2 = float(input())
     result = multiplicar(num1, num2)
     print(num1, "x", num2, "=", result)
 
-if operacao_menu == 4:
+elif operacao_menu == 4:
     num1 = float(input("Digite os numeros da dividir: "))
     num2 = float(input())
     result = dividir(num1, num2)
