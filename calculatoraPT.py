db3006ad51f890dfc53665eaac73dd032088298e
Henrique Campos <henriquecamposc2006@gmail.com>
2026-05-28 14:07:29 -0300

    feature: calculator's home page added

---
+++ calculatoraPT.py
@@ -0,0 +1,14 @@
+print(19*"=")
+print(" MENU CALCULADORA")
+print(19*"=")
+print("\n\n Selecione para a operação:\n",
+    "1. Somar\n"
+    "2. Subtrair\n"
+    "3. Multiplicar\n"
+    "4. Dividir\n"
+    "5. Raiz quadrada\n\n"
+    
+    "0. Sair\n")
+
+operacao_menu = int(input())
+print(operacao_menu)
